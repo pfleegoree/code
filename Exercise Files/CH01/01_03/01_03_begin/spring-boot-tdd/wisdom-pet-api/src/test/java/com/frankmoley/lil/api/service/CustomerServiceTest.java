f2d6b6974c613a903cae17b1928e437072a21f11
Elena Pfleegor <elena@MacBook-Pro-6.lan>
2024-08-23 16:18:00 -0500

    Testing with mockito and full coverage

--- Exercise Files/CH01/01_03/01_03_begin/spring-boot-tdd/wisdom-pet-api/src/test/java/com/frankmoley/lil/api/service/CustomerServiceTest.java
+++ Exercise Files/CH01/01_03/01_03_begin/spring-boot-tdd/wisdom-pet-api/src/test/java/com/frankmoley/lil/api/service/CustomerServiceTest.java
@@ -2,6 +2,8 @@ package com.frankmoley.lil.api.service;
 
 import com.frankmoley.lil.api.data.entity.CustomerEntity;
 import com.frankmoley.lil.api.data.repository.CustomerRepository;
+import com.frankmoley.lil.api.web.error.ConflictException;
+import com.frankmoley.lil.api.web.error.NotFoundException;
 import com.frankmoley.lil.api.web.model.Customer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,9 +13,14 @@ import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(org.mockito.junit.jupiter.MockitoExtension.class)
 public class CustomerServiceTest {
@@ -40,6 +47,66 @@ public class CustomerServiceTest {
 
         }
         return customers;
+    }
+    @Test
+    void getCustomer(){
+        CustomerEntity entity = getMockCustomerEntity();
+        Optional<CustomerEntity> optional = Optional.of(entity);
+        Mockito.doReturn(optional).when(customerRepository).findById(entity.getCustomerId());
+
+        Customer customer = this.customerService.getCustomer(entity.getCustomerId().toString());
+        assertNotNull(customer);
+        assertEquals("testFirst", customer.getFirstName());
+    }
+    @Test
+    void getCustomer_NotExist(){
+        CustomerEntity entity = getMockCustomerEntity();
+        Optional<CustomerEntity> optional = Optional.empty();
+        Mockito.doReturn(optional).when(customerRepository).findById(entity.getCustomerId());
+        assertThrows(NotFoundException.class, () ->customerService.getCustomer(entity.getCustomerId().toString()));
+    }
+    @Test
+    void findByEmailAddress(){
+        CustomerEntity entity = getMockCustomerEntity();
+        Optional<CustomerEntity> optional = Optional.of(entity);
+        Mockito.doReturn(optional).when(customerRepository).findByEmailAddress(entity.getEmailAddress());
+
+        Customer customer = this.customerService.findByEmailAddress(entity.getEmailAddress());
+        assertNotNull(customer);
+        assertEquals("testFirst", customer.getFirstName());
+    }
+    @Test
+    void addCustomer_existing(){
+        CustomerEntity entity = getMockCustomerEntity();
+        when(customerRepository.findByEmailAddress(entity.getEmailAddress())).thenReturn(entity);
+        Customer customer = new Customer(entity.getCustomerId().toString(), entity.getFirstName(), entity.getLastName(),
+                entity.getEmailAddress(), entity.getPhoneNumber(), entity.getAddress());
+       assertThrows(ConflictException.class, ()-> customerService.addCustomer(customer));
+    }
+
+    @Test
+    void updateCustomer(){
+        CustomerEntity entity = getMockCustomerEntity();
+        when(customerRepository.save(any(CustomerEntity.class))).thenReturn(entity);
+        Customer customer = new Customer(entity.getCustomerId().toString(), entity.getFirstName(), entity.getLastName(),
+                entity.getEmailAddress(), entity.getPhoneNumber(), entity.getAddress());
+        customer = customerService.updateCustomer(customer);
+        assertNotNull(customer);
+        assertNotNull("testLast", customer.getCustomerId());
+    }
+
+    @Test
+    void deleteCustomer(){
+        UUID    id = UUID.randomUUID();
+        doNothing().when(customerRepository).deleteById(id);
+        customerService.deleteCustomer(id.toString());
+    }
+
+    private CustomerEntity getMockCustomerEntity(){
+        return new CustomerEntity(UUID.randomUUID(),
+                "testFirst", "testLast",
+                "email@test.com", "111-5555-0505",
+                "1111 Main Street");
     }
 
 
--- Exercise Files/CH01/01_04/01_04_begin/spring-boot-tdd/wisdom-pet-api/src/test/java/com/frankmoley/lil/api/WisdomPetApiApplicationTests.java
+++ Exercise Files/CH01/01_04/01_04_begin/spring-boot-tdd/wisdom-pet-api/src/test/java/com/frankmoley/lil/api/WisdomPetApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class WisdomPetApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void getCustomer() {
 	}
 
 }
