66af9cc8acb1d9f641564376c1a5db079cd5580a
Elena Pfleegor <elena@MacBook-Pro-6.lan>
2024-08-29 15:10:01 -0500

    Create REST controller

--- Ex_Files_First_Spring_Boot_Microservice/Exercise Files/Chapter 2/02_06/begin/explorecali/src/main/java/com/example/ec/repo/TourPackageRepository.java
+++ Ex_Files_First_Spring_Boot_Microservice/Exercise Files/Chapter 2/02_06/begin/explorecali/src/main/java/com/example/ec/repo/TourPackageRepository.java
@@ -3,8 +3,6 @@ package com.example.ec.repo;
 import com.example.ec.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 /**
  * Tour Package Repository Interface
  *
--- Ex_Files_First_Spring_Boot_Microservice/Exercise Files/Chapter 3/03_03/begin/explorecali/src/main/java/com/example/ec/repo/TourRepository.java
+++ Ex_Files_First_Spring_Boot_Microservice/Exercise Files/Chapter 3/03_03/begin/explorecali/src/main/java/com/example/ec/repo/TourRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 
@@ -13,6 +14,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
  *
  * Created by Mary Ellen Bowman
  */
+
 public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
     Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 
