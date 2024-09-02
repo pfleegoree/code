ac9ccb4d92cec4955707b7682eaf44820c700374
Elena Pfleegor <elena@MacBook-Pro-6.lan>
2024-09-02 14:54:22 -0500

    Praccticing Springboot

--- Ex_Files_First_Spring_Boot_Microservice/Exercise Files/Chapter 2/02_02/explorecali/src/main/java/com/example/ec/repo/TourPackageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ec.repo;
-
-import com.example.ec.domain.TourPackage;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
-}
