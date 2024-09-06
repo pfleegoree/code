7e3210ce32d85f8efa2842aaa94aa7e75af0427e
Elena Pfleegor <elena@MacBook-Pro-6.lan>
2024-09-06 15:15:48 -0500

    Added Junit Test case

---
+++ MarsRoverApi/src/main/java/com/coderscampus/response/MarsPhoto.java
@@ -0,0 +1,47 @@
+package com.coderscampus.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class MarsPhoto {
+	private Long id;
+	private Integer sol;
+	@JsonProperty("mars_camera")
+	private MarsCamera marsCamera;
+	@JsonProperty("img_src")
+	private String imgSrc;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getSol() {
+		return sol;
+	}
+	public void setSol(Integer sol) {
+		this.sol = sol;
+	}
+	public MarsCamera getMarsCamera() {
+		return marsCamera;
+	}
+	public void setMarsCamera(MarsCamera marsCamera) {
+		this.marsCamera = marsCamera;
+	}
+	public String getImgSrc() {
+		return imgSrc;
+	}
+	public void setImgSrc(String imgSrc) {
+		this.imgSrc = imgSrc;
+	}
+	@Override
+	public String toString() {
+		return "MarsPhoto [id=" + id + ", sol=" + sol + ", marsCamera=" + marsCamera + ", imgSrc=" + imgSrc + "]";
+	}
+	
+	
+	
+	
+
+}
