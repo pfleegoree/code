bf81782cdd28be35f5a3323494f221a52a4e3345
Elena Pfleegor <elena@MacBook-Pro-6.lan>
2024-09-09 13:06:11 -0500

    Adding Api Service

--- MarsRoverApi/src/main/java/com/coderscampus/response/MarsRover.java
+++ MarsRoverApi/src/main/java/com/coderscampus/response/MarsRover.java
@@ -1,23 +1,43 @@
 package com.coderscampus.response;
 
-
-
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class MarsRover {
-	private Long id;
-    private String name;
-    @JsonProperty("landing_date")
-    private Date landingDate;
-    @JsonProperty("launch_date")
-    private Date launchDate;
-
-    private String active;
-    "max_sol": 4102,
-    "max_date": "2024-02-19",
-    "total_photos": 695670,
-    "cameras": [
-
+  private Long id;
+  private String name;
+  @JsonProperty("landing_date")
+  private String landingDate;
+  @JsonProperty("launch_date")
+  private String launchDate;
+  
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getLandingDate() {
+    return landingDate;
+  }
+  public void setLandingDate(String landingDate) {
+    this.landingDate = landingDate;
+  }
+  public String getLaunchDate() {
+    return launchDate;
+  }
+  public void setLaunchDate(String launchDate) {
+    this.launchDate = launchDate;
+  }
+  @Override
+  public String toString() {
+    return "MarsRover [id=" + id + ", name=" + name + ", landingDate=" + landingDate + "]";
+  }
 }
