287b4e6356f0a13983f0e06b3f774b61d0638a21
Elena Pfleegor <elena@MacBook-Pro-6.lan>
2024-09-09 23:15:10 -0500

    Added GetMapping annotation

--- MarsRoverApi/src/main/java/com/coderscampus/web/HomeController.java
+++ MarsRoverApi/src/main/java/com/coderscampus/web/HomeController.java
@@ -3,24 +3,31 @@ package com.coderscampus.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.coderscampus.response.MarsRoverApiResponse;
 import com.coderscampus.service.MarsRoverApiService;
 
+
+
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private MarsRoverApiService roverService;
-	
-	@GetMapping(value="/")
-	public String getHomeView(ModelMap model) {
-	MarsRoverApiResponse roverData	= roverService.getRoverData("opportunity");
-	model.put("roverData", roverData);
 
-		return "index";
-	}
-	
-
+  @Autowired
+  private MarsRoverApiService roverService;
+  
+  @GetMapping("/")
+  public String getHomeView (ModelMap model, @RequestParam(required=false) String marsApiRoverData) {
+    // if request param is empty, then set a default value
+    if (!StringUtils.hasLength(marsApiRoverData)) {
+      marsApiRoverData = "opportunity";
+    }
+    MarsRoverApiResponse roverData = roverService.getRoverData(marsApiRoverData);
+    model.put("roverData", roverData);
+    
+    return "index";
+  }
+  
 }
