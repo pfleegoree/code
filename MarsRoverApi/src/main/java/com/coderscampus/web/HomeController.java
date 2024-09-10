d9cbe2b81da7c060279c118c8c26c2469ccd7c6b
Elena Pfleegor <elena@MacBook-Pro-6.lan>
2024-09-10 12:34:41 -0500

    Added Label Imput for Filtering the day

--- MarsRoverApi/src/main/java/com/coderscampus/web/HomeController.java
+++ MarsRoverApi/src/main/java/com/coderscampus/web/HomeController.java
@@ -19,12 +19,18 @@ public class HomeController {
   private MarsRoverApiService roverService;
   
   @GetMapping("/")
-  public String getHomeView (ModelMap model, @RequestParam(required=false) String marsApiRoverData) {
+  public String getHomeView (ModelMap model, @RequestParam(required=false) String marsApiRoverData,
+		  @RequestParam(required=false) Integer marsSol) {
     // if request param is empty, then set a default value
     if (!StringUtils.hasLength(marsApiRoverData)) {
       marsApiRoverData = "opportunity";
     }
-    MarsRoverApiResponse roverData = roverService.getRoverData(marsApiRoverData);
+    if(marsSol == null) {
+    	marsSol = 1;
+    }
+    	
+    
+    MarsRoverApiResponse roverData = roverService.getRoverData(marsApiRoverData, marsSol);
     model.put("roverData", roverData);
     
     return "index";
