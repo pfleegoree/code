a95bd81c6cb37990c87636733a90cd63690b3b6e
Elena Pfleegor <elena@MacBook-Pro-6.lan>
2024-09-12 17:36:31 -0500

    Adding get Element by ID

--- MarsRoverApi/src/main/java/com/coderscampus/repository/PreferencesRepository.java
+++ MarsRoverApi/src/main/java/com/coderscampus/repository/PreferencesRepository.java
@@ -1,9 +1,11 @@
 package com.coderscampus.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.coderscampus.dto.HomeDto;
 
 public interface PreferencesRepository extends JpaRepository<HomeDto, Long>{
+
+//  @Query(value="select * from mars_api_preferences where user_id = :userId", nativeQuery=true)
+	HomeDto findByUserId(Long userId);
 
 }
