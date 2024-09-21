1d4ff5af8b5e4a1d639cfa627906de86d68c4d34
Elena Pfleegor <elena@MacBook-Pro-6.lan>
2024-09-20 20:53:10 -0500

    Added Login Model and Password

--- Bookstore/src/main/java/com/bookstore/config/SecurityConfig.java
+++ Bookstore/src/main/java/com/bookstore/config/SecurityConfig.java
@@ -1,13 +1,8 @@
 package com.bookstore.config;
 
-import com.bookstore.service.impl.UserSecurityService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bookstore.utility.SecurityUtility;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
@@ -19,16 +14,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private Environment env;
 
-    @Autowired
-    private UserSecurityService userSecurityService;
 
     //keep
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    private BCryptPasswordEncoder passwordEncoder() {
+        return SecurityUtility.passwordEncoder();
     }
 
 
@@ -43,16 +33,10 @@ public class SecurityConfig {
 
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers(PUBLIC_MATCHERS).hasAnyRole()
+                        .requestMatchers(PUBLIC_MATCHERS).hasAnyRole("USER")
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
@@ -64,9 +48,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
-    }
+
 
 }
