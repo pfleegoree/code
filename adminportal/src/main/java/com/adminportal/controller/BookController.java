79d269513a9e14e078aa982d1f4226c9c9973fd1
Elena Pfleegor <elena@MacBook-Pro-6.lan>
2024-10-10 15:33:18 -0500

    Fixed Image Path. Servlett  issue

--- adminportal/src/main/java/com/adminportal/controller/BookController.java
+++ adminportal/src/main/java/com/adminportal/controller/BookController.java
@@ -2,7 +2,6 @@ package com.adminportal.controller;
 
 import com.adminportal.domain.Book;
 import com.adminportal.service.BookService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -30,22 +30,20 @@ public class BookController {
         return "addBook";
     }
 
-    @RequestMapping(value="/add", method= RequestMethod.POST)
-    public String addBookPost(
-            @ModelAttribute("book") Book book, HttpServletRequest request
-            ){
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addBookPost(@ModelAttribute("book") Book book, HttpServletRequest request) {
         bookService.save(book);
 
         MultipartFile bookImage = book.getBookImage();
 
         try {
             byte[] bytes = bookImage.getBytes();
-            String name = book.getId() + " .png";
+            String name = book.getId() + ".png";
             BufferedOutputStream stream = new BufferedOutputStream(
-                    new FileOutputStream(new File("/src/main/resources/static/image/book" + name)));
+                    new FileOutputStream(new File("src/main/resources/static/image/book/" + name)));
             stream.write(bytes);
             stream.close();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
